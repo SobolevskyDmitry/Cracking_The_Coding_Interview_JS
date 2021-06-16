c2cf87ae5ec46aea0818c44fe0c4784ef49cd630
dmitry_sobolevsky <dmitry.sobolevsky@insparx.com>
2021-06-16 13:19:46 +0200

    Chapter 04: 4.5

--- Chapter_04_Graphs&BT/4.5_Is_Valid_BST.js
+++ Chapter_04_Graphs&BT/4.5_Is_Valid_BST.js
@@ -3,8 +3,8 @@ const {createBst} = require("./utils");
 let bst = createBst([1, 2, 3, 4, 5, 6, 7]);
 
 /**
-        4
-    2        6
+ 4
+ 2        6
  1     3  5     7
 
  **/
@@ -34,4 +34,22 @@ function isValidBst(root) {
 }
 
 
-console.log(isValidBst(bst));
+// another approach with min & max
+function checkBst(n, min, max) {
+    if (!n) {
+        return true;
+    }
+
+    if (min && n.data <= min || max && n.data > max) {
+        return false;
+    }
+
+    if (!checkBst(n.left, min, n.data) || !checkBst(n.right, n.data, max)) {
+        return false;
+    }
+
+    return true;
+}
+
+
+console.log(checkBst(bst));
