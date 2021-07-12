d8e42434df2fa23b4b66fca0ec75a18eb25fc4bb
dmitry_sobolevsky <dmitry.sobolevsky@insparx.com>
2021-07-12 17:52:26 +0200

    Chapter 10: Fancy mergeSort

--- Chapter_10_Sorts/10.6_MergeSort.js
+++ Chapter_10_Sorts/10.6_MergeSort.js
@@ -42,3 +42,38 @@ function merge(leftArr, rightArr) {
 }
 
 mergeSort([6, 5, 12, 10, 9, 1]);
+
+
+/**
+ Fancy approach
+ **/
+
+function mergeSort(arr) {
+    const half = arr.length / 2;
+
+    // the base case is array length <=1
+    if (arr.length <= 1) {
+        return arr;
+    }
+
+    const left = arr.splice(0, half); // the first half of the array
+    const right = arr;
+    return merge(mergeSort(left), mergeSort(right));
+}
+
+
+function merge(left, right) {
+    let sortedArr = []; // the sorted elements will go here
+
+    while (left.length && right.length) {
+        // insert the smallest element to the sortedArr
+        if (left[0] < right[0]) {
+            sortedArr.push(left.shift());
+        } else {
+            sortedArr.push(right.shift());
+        }
+    }
+
+    // use spread operator and create a new array, combining the three arrays
+    return [...sortedArr, ...left, ...right];
+}
